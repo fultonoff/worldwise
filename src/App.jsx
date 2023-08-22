ec9113a43d9236ec95ed2843fe81b9d1653d2a15
fultonoff <fultonkilebe7@gmail.com>
2023-08-22 23:13:33 +0200

    ✅aaded go back

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react"
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
 import HomePage from "./pages/homePage"
 import Pricing from "./pages/pricing"
 import Product from "./pages/product"
@@ -44,7 +44,7 @@ const App = () => {
         <Route path="login" element={<Login/>}/>
         <Route path="app" element={<AppLayout/>}>
 
-          <Route index element={<CityList cities={cities} isLoading={isLoading}/>}/>
+          <Route index element={<Navigate replace to='cities'/>}/>
           <Route path='cities' element={<CityList cities={cities} isLoading={isLoading}/>}/>
           <Route path="cities/:id" element={<City/>}/>
           <Route path='countries' element={<CountryList cities={cities} isLoading={isLoading}/>}/>
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import styles from './Button.module.css'
 
 const Button = ({children, onClick, type}) => {
